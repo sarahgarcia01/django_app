e11c4bb2bef5eaad00108c636075f8a0812b7045
Anth0ny-Magana <101376259+Anth0ny-Magana@users.noreply.github.com>
2023-12-03 18:54:43 -0600

    Put roger's and sarah's code together

--- django_app/urls.py
+++ django_app/urls.py
@@ -21,14 +21,23 @@ from website.views import index
 from django.conf.urls.static import static 
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='homepage'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
-    path('searchresults/', views.searchresults, name='searchresult'),
-    
+    path('searchresults/', views.searchresults, name='searchresults'),
+    path('categoryresults/<str:category>/', views.categoryresults, name='categoryresults'),
+    path('shopping_cart/', views.shopping_cart, name='shopping_cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/<int:product_id>/', views.update_cart, name='update_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('place_order/', views.place_order, name='place_order'),
+    path('order_complete/', views.order_complete, name='order_complete'),
+    path('product/<int:product_id>/', views.productview, name='productview'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
